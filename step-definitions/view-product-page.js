c64b404a8b19473ad66fa1a3e1c67a9de258c3d7
cjtornberg96 <carljohantornberg96@gmail.com>
2022-01-05 14:31:18 +0100

    view-product-page.js

---
+++ step-definitions/view-product-page.js
@@ -0,0 +1,12 @@
+module.exports = function () {
+
+  this.When(/^the user clicks on a product$/, async function () {
+
+    let veckansVarorLink = await driver.findElement(By.css('button[class="ax-product-puff-image selenium--product-puff-img layout-align-center"]'));
+    await veckansVarorLink.click();
+
+
+
+  })
+
+}
