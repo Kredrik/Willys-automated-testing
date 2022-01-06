c167657ea926e944b314818f3bb696a79deb4d6e
cjtornberg96 <carljohantornberg96@gmail.com>
2022-01-06 17:03:46 +0100

    Updated variables for view-product

--- step-definitions/view-product-page.js
+++ step-definitions/view-product-page.js
@@ -2,14 +2,13 @@ module.exports = function () {
 
   this.When(/^the user clicks on a product$/, async function () {
 
-    let veckansVarorImg = await driver.findElement(By.css('img.resized-image'));
-    await veckansVarorImg.click();
+    let frontPageProduct = await driver.findElement(By.css('img.resized-image'));
+    await frontPageProduct.click();
   })
 
   this.Then(/^the user should be shown an overview of the product and different tabs$/, async function () {
 
     await driver.sleep(5000);
-
     let h1Text;
 
     h1Text = await (await driver.findElement(By.css('.product-detail-title'))).getText();
@@ -19,8 +18,8 @@ module.exports = function () {
 
   this.Then(/^the user clicks the övrigt-tab to view information about the product$/, async function () {
 
-    let allTabs = await driver.findElement(By.css('div[ax-analytics2-action="ProductPage_ProductInfo_OtherInfo"]'));
-    await allTabs.click();
+    let otherInfoTab = await driver.findElement(By.css('div[ax-analytics2-action="ProductPage_ProductInfo_OtherInfo"]'));
+    await otherInfoTab.click();
     await driver.sleep(3000);
 
   })
