bac2d87cb449e0b2bd5ed9323acb6e3a52d70d85
Simon Haag <simonhaag1@hotmail.com>
2022-01-06 17:03:12 +0100

    Updated cart-view feature and step definitions

--- step-definitions/cart-view.js
+++ step-definitions/cart-view.js
@@ -2,9 +2,27 @@ module.exports = function () {
 
   this.When(/^the user has selected products in the cart$/, async function () {
 
-    let veckansVarorLink = await driver.findElement(By.css('button[class="ax-product-puff-image selenium--product-puff-img layout-align-center"]'));
-    await veckansVarorLink.click();
+    let fruitsAndGreensMenu = await driver.findElement
+      (By.css('a[href="/sortiment/frukt-och-gront"'));
+    await fruitsAndGreensMenu.click();
 
+    await driver.wait(until.elementsLocated
+      (by.css('a[href="/sortiment/frukt-och-gront/farska-bar"')), 10000);
+
+    let freshBerriesMenu = await driver.findElement
+      (by.css('a[href="/sortiment/frukt-och-gront/farska-bar"'));
+
+    await driver.executeScript
+      ('document.querySelector(\'a[href="/sortiment/frukt-och-gront/farska-bar"]\').scrollIntoView()');
+    await freshBerriesMenu.click();
+
+    await driver.sleep(3000);
+
+    let h2Text;
+    while (h2Text !== 'Färska bär') {
+      h2Text = await (await driver.findElement(By.css('h2'))).getText();
+      await driver.sleep(100);
+    }
 
 
   })
