f0ac6e96a6d1c8eededdeb639fe3afe0dad928b2
cjtornberg96 <carljohantornberg96@gmail.com>
2022-01-06 19:15:58 +0100

    Complete cart-view step-definition

--- step-definitions/cart-view.js
+++ step-definitions/cart-view.js
@@ -12,6 +12,7 @@ module.exports = function () {
     let freshBerriesMenu = await driver.findElement
       (by.css('a[href="/sortiment/frukt-och-gront/farska-bar"'));
 
+    await driver.sleep(2000);
     await driver.executeScript
       ('document.querySelector(\'a[href="/sortiment/frukt-och-gront/farska-bar"]\').scrollIntoView()');
     await freshBerriesMenu.click();
@@ -24,7 +25,48 @@ module.exports = function () {
       await driver.sleep(100);
     }
 
+    let products = await driver.findElements(By.css('[itemtype="https://schema.org/Product"]'));
+
+    for (let product of products) {
+
+      let productStrawberry = (await product.getText()).includes('Jordgubbar Klass 1');
+      let productBlueberry = (await product.getText()).includes('Blåbär Klass 1');
+      let productRaspberry = (await product.getText()).includes('Hallon Klass 1');
+      if (!productStrawberry && !productBlueberry && !productRaspberry) { continue }
+
+      let buyProduct = await (await product.findElement(By.css('button[title="Öka antal"]')));
+      await buyProduct.click();
+
+      await driver.sleep(2000);
+
+    }
 
   })
+
+  this.When(/^the user clicks on the cart icon$/, async function () {
+
+    let goToCart = (await driver.findElement(By.css('a[href="https://www.willys.se/varukorg"]')));
+    await goToCart.click();
+    await driver.sleep(2000);
+
+  });
+
+  this.Then(/^the user should see the selected products$/, async function () {
+    let checkCartView = (await driver.findElement(By.css('p[class="Text_text__1DvUt Text_body__FrvAc Text_black__yB42g Text_small__3m75u"')).getText());
+    expect(checkCartView).to.be.equal('Totalt (3)');
+    await driver.sleep(2000);
+  });
+
+  this.Then(/^the user will have the option to buy them$/, async function () {
+
+    let goToCheckOut = (await driver.findElement(By.css('button[class="Buttonstyles__StyledButton-sc-1g4oxwr-0 gqUoiG"]')));
+    await goToCheckOut.click();
+    await driver.sleep(2000);
+
+    let CheckOut = (await driver.findElement(By.css('span[class="checkout-button-text"]')));
+    await CheckOut.click();
+    await driver.sleep(2000);
+
+  });
 
 };
