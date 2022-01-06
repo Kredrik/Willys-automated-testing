e9ea9feacf1f933e7278e786fb36a59ccb348edf
Simon Haag <simonhaag1@hotmail.com>
2022-01-06 19:19:47 +0100

    Cleaned up the code a bit!

--- step-definitions/cart-view.js
+++ step-definitions/cart-view.js
@@ -11,12 +11,11 @@ module.exports = function () {
 
     let freshBerriesMenu = await driver.findElement
       (by.css('a[href="/sortiment/frukt-och-gront/farska-bar"'));
-
     await driver.sleep(2000);
+
     await driver.executeScript
       ('document.querySelector(\'a[href="/sortiment/frukt-och-gront/farska-bar"]\').scrollIntoView()');
     await freshBerriesMenu.click();
-
     await driver.sleep(3000);
 
     let h2Text;
@@ -25,7 +24,8 @@ module.exports = function () {
       await driver.sleep(100);
     }
 
-    let products = await driver.findElements(By.css('[itemtype="https://schema.org/Product"]'));
+    let products = await driver.findElements
+      (By.css('[itemtype="https://schema.org/Product"]'));
 
     for (let product of products) {
 
@@ -34,9 +34,9 @@ module.exports = function () {
       let productRaspberry = (await product.getText()).includes('Hallon Klass 1');
       if (!productStrawberry && !productBlueberry && !productRaspberry) { continue }
 
-      let buyProduct = await (await product.findElement(By.css('button[title="Öka antal"]')));
+      let buyProduct = await (await product.findElement
+        (By.css('button[title="Öka antal"]')));
       await buyProduct.click();
-
       await driver.sleep(2000);
 
     }
@@ -45,26 +45,30 @@ module.exports = function () {
 
   this.When(/^the user clicks on the cart icon$/, async function () {
 
-    let goToCart = (await driver.findElement(By.css('a[href="https://www.willys.se/varukorg"]')));
+    let goToCart = (await driver.findElement
+      (By.css('a[href="https://www.willys.se/varukorg"]')));
     await goToCart.click();
     await driver.sleep(2000);
 
   });
 
   this.Then(/^the user should see the selected products$/, async function () {
-    let checkCartView = (await driver.findElement(By.css('p[class="Text_text__1DvUt Text_body__FrvAc Text_black__yB42g Text_small__3m75u"')).getText());
+    let checkCartView = (await driver.findElement
+      (By.css('p[class="Text_text__1DvUt Text_body__FrvAc Text_black__yB42g Text_small__3m75u"')).getText());
     expect(checkCartView).to.be.equal('Totalt (3)');
     await driver.sleep(2000);
   });
 
   this.Then(/^the user will have the option to buy them$/, async function () {
 
-    let goToCheckOut = (await driver.findElement(By.css('button[class="Buttonstyles__StyledButton-sc-1g4oxwr-0 gqUoiG"]')));
+    let goToCheckOut = (await driver.findElement
+      (By.css('button[class="Buttonstyles__StyledButton-sc-1g4oxwr-0 gqUoiG"]')));
     await goToCheckOut.click();
     await driver.sleep(2000);
 
-    let CheckOut = (await driver.findElement(By.css('span[class="checkout-button-text"]')));
-    await CheckOut.click();
+    let checkOut = (await driver.findElement(
+      By.css('span[class="checkout-button-text"]')));
+    await checkOut.click();
     await driver.sleep(2000);
 
   });
