cccfadb15a2b10cbadb623ee2adb0a03713d0411
cjtornberg96 <carljohantornberg96@gmail.com>
2022-01-06 12:53:30 +0100

    Changes to view-product script

--- step-definitions/view-product-page.js
+++ step-definitions/view-product-page.js
@@ -2,10 +2,30 @@ module.exports = function () {
 
   this.When(/^the user clicks on a product$/, async function () {
 
-    //let veckansVarorLink = await driver.findElement(By.css('button[class="ax-product-puff-image selenium--product-puff-img layout-align-center"]'));
-    //await veckansVarorLink.click();
+    let veckansVarorImg = await driver.findElement(By.css('img.resized-image'));
+    await veckansVarorImg.click();
+  })
+
+  this.Then(/^the user should be shown an overview of the product$/, async function () {
+
+    /*
+    await driver.sleep(3000);
+    let clickTabOvrigt = await driver.findElement(By.css('#tab-item-326'));
+    await clickTabOvrigt.click();
+
+    await driver.sleep(3000);
 
 
+
+    
+    let h1Text;
+    while (h1Text !== 'Majskyckling Hel Sverige') {
+      h1Text = await (await driver.findElement(By.css('h1'))).getText();
+      await driver.sleep(100);
+    }
+
+    await driver.sleep(3000);
+    */
 
   })
 
