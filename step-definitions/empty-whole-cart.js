be38419d1b3cf80f3c7a872fb213ef4e48d870c2
Simon Haag <simonhaag1@hotmail.com>
2022-01-07 13:20:12 +0100

    Updated empty-whole-cart step definition

--- step-definitions/empty-whole-cart.js
+++ step-definitions/empty-whole-cart.js
@@ -23,20 +23,16 @@ module.exports = function () {
 
   this.When(/the user click the empty button$/, async function () {
 
-    let emptyButton = await driver.findElements(By.css('button[ax-analytics2-action="MiniCart_CleanCart"]'));
+    let emptyButton = await driver.findElement(By.css('button[ax-analytics2-action="MiniCart_CleanCart"]'));
     await emptyButton.click();
     await driver.wait(until.elementsLocated(By.css('button[ax-analytics2-action="MiniCart_CleanCart"]')));
     await driver.sleep(3000);
   });
 
   this.When(/the user agree to the popup that assures that the user want to empty the cart$/, async function () {
-    let button = await driver.findElements(By.css('document.querySelector("#selenium--miniCart-empty-cart-btn > span")'));
+    let button = await (await driver.findElements(By.css('div[md-ripple-container]')[1]));
+    await driver.wait(until.elementsLocated(By.css('div[md-ripple-container]')));
     await button.click();
-    await driver.wait(untill.elementsLocated(By.css('document.querySelector("#selenium--miniCart-empty-cart-btn > span")')));
-    await driver.sleep(3000);
   })
-
-
-
 
 };
