4b86de20a088c2dfa70d801be9fccd091d6ed022
Simon Haag <simonhaag1@hotmail.com>
2022-01-14 11:12:59 +0100

    Changed back to original step-definition

--- step-definitions/adding-multiple-products.js
+++ step-definitions/adding-multiple-products.js
@@ -9,32 +9,30 @@ module.exports = function () {
       (by.css('input[placeholder="Sök i e-handeln"]'));
   });
 
-  this.When
-    (/^adds "([^"]*)", "([^"]*)", "([^"]*)", "([^"]*)", "([^"]*)", "([^"]*)" to the cart$/,
-      //async function (baguette, butter, tuna, majo, ruccola, tomatoes) {
-      async function (...ingredients) {
-        //ingredients.push(baguette, butter, tuna, majo, ruccola, tomatoes);
-        let plusIcon;
-        for (let ingredient of ingredients) {
-          await driver.wait(until.elementsLocated
-            (by.css('input[placeholder="Sök i e-handeln"]')), 10000);
-          searchField = await driver.findElement
-            (by.css('input[placeholder="Sök i e-handeln"]'));
-          await driver.sleep(2000);
-          await searchField.sendKeys(ingredient, selenium.Key.ENTER);
+  this.When(/^adds "([^"]*)", "([^"]*)", "([^"]*)", "([^"]*)", "([^"]*)", "([^"]*)" to the cart$/,
+    async function (baguette, butter, tuna, majo, ruccola, tomatoes) {
+      ingredients.push(baguette, butter, tuna, majo, ruccola, tomatoes);
+      let plusIcon;
+      for (let ingredient of ingredients) {
+        await driver.wait(until.elementsLocated
+          (by.css('input[placeholder="Sök i e-handeln"]')), 10000);
+        searchField = await driver.findElement
+          (by.css('input[placeholder="Sök i e-handeln"]'));
+        await driver.sleep(2000);
+        await searchField.sendKeys(ingredient, selenium.Key.ENTER);
 
-          await driver.sleep(2000);
-          await driver.wait(until.elementsLocated
-            (by.css('button[title="Öka antal"]')), 10000);
-          plusIcon = await (await driver.findElement
-            (by.css('button[title="Öka antal"]')));
-          await plusIcon.click();
-        }
+        await driver.sleep(2000);
+        await driver.wait(until.elementsLocated
+          (by.css('button[title="Öka antal"]')), 10000);
+        plusIcon = await (await driver.findElement
+          (by.css('button[title="Öka antal"]')));
+        await plusIcon.click();
+      }
 
-        let miniCart = (await driver.findElement
-          (by.css('button[class*="MiniCartButton"]')));
-        await miniCart.click();
-      });
+      let miniCart = (await driver.findElement
+        (by.css('button[class*="MiniCartButton"]')));
+      await miniCart.click();
+    });
 
 
   this.Then(/^the user should have multiple products in the cart$/, async function () {
